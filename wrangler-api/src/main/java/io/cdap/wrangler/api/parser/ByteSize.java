7c258dd43025fabbc5e8cc214708bbc0119648f6
somya bhatt <somyacu1@gmail.com>
2025-04-12 20:53:58 +0530

    feat: Add ByteSize and TimeDuration token classes

---
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/ByteSize.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2017-2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+
+package io.cdap.wrangler.api.parser;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+public class ByteSize implements Token {
+  private final long bytes;
+
+  public ByteSize(String bytes) {
+    this.bytes = convertToBytes(bytes);
+  }
+
+  private long convertToBytes(String bytes){
+    String unit = bytes.substring(bytes.length() - 1).toUpperCase();
+    long value = Long.parseLong(bytes.substring(0, bytes.length() - 1));
+    switch (unit) {
+      case "B":
+        return value;
+      case "K":
+        return value * 1024;
+      case "M":
+        return value * 1024 * 1024;
+      case "G":
+        return value * 1024 * 1024 * 1024;
+      case "T":
+        return value * 1024 * 1024 * 1024 * 1024;
+      default:
+        throw new IllegalArgumentException("Invalid unit: " + unit);
+    }
+  }
+
+  @Override
+  public Object value() {
+    return bytes;
+  }
+
+  @Override
+  public TokenType type() {
+    return TokenType.BYTE_SIZE;
+  }
+
+  @Override
+  public JsonElement toJson() {
+    JsonObject object = new JsonObject();
+    object.addProperty("type", TokenType.BYTE_SIZE.name());
+    object.addProperty("value", bytes);
+    return object;
+  }
+
+  public long getBytes() {
+    return bytes;
+  }
+}
--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
@@ -152,5 +152,18 @@ public enum TokenType implements Serializable {
    * Represents the enumerated type for the object of type {@code String} with restrictions
    * on characters that can be present in a string.
    */
-  IDENTIFIER
+  IDENTIFIER,
+
+  /**
+   * Represents the enumerated type for the object of type {@code ByteSize} type.
+   * This type is associated with the token that represents a byte size.
+   */
+  BYTE_SIZE,
+
+  /**
+   * Represents the enumerated type for the object of type {@code TimeDuration} type.
+   * This type is associated with the token that represents a time duration.
+   */
+  TIME_DURATION
+
 }
